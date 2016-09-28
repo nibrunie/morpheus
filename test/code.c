aa648f7a0c20a62b66f1257a2f175d42dcfe18b0
Nicolas Brunie <nibrunie@gmail.com>
2016-09-28 15:36:56 +0200

    adding homomorphic multiplication test

--- test/code.c
+++ test/code.c
@@ -8,12 +8,21 @@
 #include "morphdebug.h"
 
 
+uint64_t emul_poly_mul(uint32_t a, uint32_t b)
+{
+  uint64_t tmp = 0;
+  for (int i = 0; i < 32; ++i)
+    if ((b >> i) & 1) tmp ^= (uint64_t) a << i;
+
+  return tmp;
+}
+
 int main() {
   morph_poly_t* a, *b, *c;
 
   morph_state_t state;
-  state.n = 32;
-  state.q = 17;
+  state.n = 256;
+  state.q = 7681;
   state.poly_mod = morph_poly_new(NULL, state.n+1);
   morph_poly_set_coeff_ui(state.poly_mod, 0, 1);
   morph_poly_set_coeff_ui(state.poly_mod, state.n, 1);
@@ -34,9 +43,7 @@ int main() {
   morph_secret_t _secret;
   morph_secret_t* secret =  &_secret;
   secret->secret_s = morph_poly_new(&state, state.n);
-  morph_poly_set_coeff_ui(secret->secret_s, 0, 1);
-  //morph_poly_sample(uni_distrib, secret->secret_s, state.n);
-  morph_poly_display("secret_s: \n", secret->secret_s, "\n");
+  morph_poly_sample(uni_distrib, secret->secret_s, state.n);
 
   secret->distrib_a = uni_distrib;
   secret->distrib_e = distrib_e;
@@ -45,10 +52,12 @@ int main() {
   morph_cipher_t* ca = morph_cipher_new(2);
   morph_cipher_t* cb = morph_cipher_new(2);
   morph_cipher_t* cc = morph_cipher_new(2);
+  morph_cipher_t* cm = morph_cipher_new(2);
 
   morph_poly_t* deca = morph_poly_new(&state, state.n);
   morph_poly_t* decb = morph_poly_new(&state, state.n);
   morph_poly_t* decc = morph_poly_new(&state, state.n);
+  morph_poly_t* decm = morph_poly_new(&state, state.n);
 
   morph_encrypt(secret, ca, a);
   morph_decrypt(secret, deca, ca);
@@ -57,13 +66,16 @@ int main() {
 
   morph_homomorphic_add(&state, cc, ca, cb);
 
+  morph_homomorphic_mult(&state, cm, ca, cb);
 
   morph_decrypt(secret, decb, cb);
   morph_decrypt(secret, decc, cc);
+  morph_decrypt(secret, decm, cm);
 
   uint32_t da = morph_decode_u32(&state, deca);
   uint32_t db = morph_decode_u32(&state, decb);
   uint32_t dc = morph_decode_u32(&state, decc);
+  uint32_t dm = morph_decode_u32(&state, decm);
 
   printf("ua = %"PRIx32" => da=%"PRIx32" \n", ua, da);
   assert(ua == da && "encode/decode failed");
@@ -73,6 +85,10 @@ int main() {
 
   printf("homomorphic ua ^ ub = %"PRIx32" vs %"PRIx32" \n", dc, ua ^ ub);
   assert(dc == (ua ^ ub) && "homomorphic add failed");
+
+  uint32_t emul_dm = emul_poly_mul(ua, ub);
+
+  printf("homomorphic ua x ub = %"PRIx32" vs %"PRIx32" \n", dm, emul_dm);
 
 
 
